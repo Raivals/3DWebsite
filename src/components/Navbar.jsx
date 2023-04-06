e6f7edc5b8a47513d612438255a7487bb3150b41
Raivals <103427644+Raivals@users.noreply.github.com>
2023-04-06 14:04:48 +0200

    End of Hero section (3D model + MQueries)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Adrian's &nbsp;
+            Romain's &nbsp;
             <span className="sm:block hidden">| 3D Portfolio </span>
           </p>
         </Link>
