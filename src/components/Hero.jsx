1c36aac32b91350945525ef16b70f3d75235b958
Raivals <103427644+Raivals@users.noreply.github.com>
2023-06-20 13:03:27 +0200

    Delete computer 3D

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,8 @@
-import { motion } from "framer-motion"
-
 import { styles } from "../styles"
-import { ComputersCanvas } from "./canvas"
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-[700px] mx-auto">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl flex flex-row items-start gap-5`}
       >
@@ -23,24 +20,6 @@ const Hero = () => {
             je conçois l’expérience utilisateur d’interfaces web.
           </p>
         </div>
-      </div>
-
-      <ComputersCanvas />
-
-      <div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-32">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.div
-              animate={{ y: [0, 24, 0] }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 mb-1 rounded-full bg-secondary"
-            />
-          </div>
-        </a>
       </div>
     </section>
   )
